8ea1a31f1f1398005c11fa804a15757d099b1de5
Ben Walcutt <bewalcut@email.uark.edu>
2015-03-31 21:57:28 -0500

    Update para6.php

--- para6.php
+++ para6.php
@@ -1,19 +1,32 @@
 <?php
 session_start();
 ?>
-
 <html>
 <title>My Paradigms Assignment</title>
-
 <body>
+<form action="<?php print($_SERVER['SCRIPT_NAME'])?>" method="get">
+<input type="text" value="" name="input_text">
+<input type="submit" value="Submit">
+</form>
 
 <form action="<?php print($_SERVER['SCRIPT_NAME'])?>" method="get">
-	<input type="text" value="" name="input_text">
-	<input type="submit" value="Submit" name="submitter">
-
+<input type="submit" value="Save" name="save">
 </form>
-<form>
-	<input type="button" value="Save" name="saver" onclick=<?php 
+<br>
+<?php
+if(!isset($_SESSION['entries']))
+$_SESSION['entries'] = array();
+$entries = &$_SESSION['entries'];
+$incoming_entry = htmlentities($_GET["input_text"]);
+$entries []= $incoming_entry;
+$arrlength = count($entries);
+
+for ($i = $arrlength - 1; $i >= 0; $i--) {
+echo $entries[$i];
+echo "<br>";
+}
+
+if(isset($_GET['save'])) {
 	$fh = fopen("entries.json", 'w');
 	if($fh === false)
 		die("Failed to open entries.json for writing.");
@@ -21,25 +34,10 @@ session_start();
 	{
 		fwrite($fh, json_encode($entries));
 		fclose($fh);
-	} ?>
->
-
-</form>
-<br>
-<?php
-if(!isset($_SESSION['entries']))
-	$_SESSION['entries'] = array();
-
-$entries = &$_SESSION['entries'];
-$incoming_entry = htmlentities($_GET["input_text"]);
-$entries []= $incoming_entry;
-$arrlength = count($entries);
-
-for ($i = $arrlength - 1; $i >= 0; $i--) {
-	echo $entries[$i];
-	echo "<br>";
+	}
 }
 //session_destroy();
 ?>
 </body>
 </html>
+
